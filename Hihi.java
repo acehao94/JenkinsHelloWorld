ddf5a8b4187fb53a2d748cafaa4be5c72ca99098
acehao94 <57552543+acehao94@users.noreply.github.com>
2019-11-23 15:07:50 +0800

    Update Hihi.java

--- Hihi.java
+++ Hihi.java
@@ -2,7 +2,7 @@ public class Hihi {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World this is a training website, printing this message");
+        System.out.println("Hello, World this is a training website, printing this message, this is using webhook");
     }
 
 }
